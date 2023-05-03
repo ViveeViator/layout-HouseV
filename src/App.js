51914a231252e8bf06608ea340c5dda184e75d20
ihorDudkevych <egorka01051993@gmail.com>
2023-05-03 13:52:20 +0300

    add (dev), sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {Header} from "./Components/Header";
 
 export const App = () => {
     return (
         <main className="App">
-            { /* сюда імпортиш компоненти*/ }
+            <Header />
+            { /* сюда імпортиш компоненти*/}
         </main>
     );
 }
