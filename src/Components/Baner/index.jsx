c966a1642c48945cb902ac3e9322958d0d88193a
ViveeViator <95189226+ViveeViator@users.noreply.github.com>
2023-06-15 00:44:51 +0300

    baner

--- src/Components/Baner/index.jsx
+++ src/Components/Baner/index.jsx
@@ -12,13 +12,13 @@ export const Baner = () => {
             
             <img src={mouse} alt={mouse} width={40} className='baner_icon_mouse' />
             <ul className='baner_list'>
-                <li>
+                <li className='baner_item'>
                     
                     <a href="http://localhost:3000/" className='baner_maps'><img src={group} alt={group} width={21} className='baner_maps_img'/>
                         Наб. реки Фонтанки 10-15</a>
                 </li>
             
-                <li>
+                <li className='baner_item'>
                     
                     <a href="tel:+8 (812) 123-45-67" className='baner_tel'><img src={phone} alt={phone} width={21} height={21} className='baner_tel_img'/> 8 (812) 123-45-67</a>
                 </li>
